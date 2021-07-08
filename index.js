1c95a6aeb173c693b0e21cf545d1e46ea086e6ba
Hp  ALONE <70450514+Alone-kurd@users.noreply.github.com>
2021-07-08 13:55:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/MauMKM7fJP`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە `) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("845727675808219196").send(
+  client.channels.get("id channel").send(
 `> :lying_face: :disguised_face:  By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
